5a5b1f14fe3f3624bf5c78278a7f9714e520811e
rtorres76 <rutorres@ipvc.pt>
2023-06-06 15:13:00 +0100

    classes necessarias

---
+++ src/main/java/com/example/gamao/casa.java
@@ -0,0 +1,10 @@
+package com.example.gamao;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class casa {
+
+
+
+}
---
+++ src/main/java/com/example/gamao/jogador.java
@@ -0,0 +1,7 @@
+package com.example.gamao;
+
+public class jogador {
+
+
+
+}
