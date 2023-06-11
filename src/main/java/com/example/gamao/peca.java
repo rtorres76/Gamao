be95701c3a72592d22b98488059ae2349161f0ac
rtorres76 <rutorres@ipvc.pt>
2023-06-11 19:31:23 +0100

    commit class peca

--- src/main/java/com/example/gamao/peca.java
+++ src/main/java/com/example/gamao/peca.java
@@ -1,7 +1,26 @@
 package com.example.gamao;
 
-public class peca {
+import java.io.Serializable;
 
+public class peca implements Serializable {
 
+    String jogador;
+
+    int posicaoX, posicaoY;
+
+/**
+    //-- CONSTRUTOR
+    //--  @param jogador jogador ao qual a peça pertence
+    //--  @param posX posição X da peça
+    //--  @param posY posição Y da peça
+ */
+
+public peca(String jogador, int posX, int posY) {
+
+    this.posicaoX = posX;
+    this.posicaoY = posY;
+    this.jogador = jogador;
+
+    }
 
 }
