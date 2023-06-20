be2b0e8e9c94cf11ed97399c0bf3b1326ba73bf7
valterelli45 <valter07082001@gmail.com>
2023-06-20 19:53:59 +0100

    gamaofxcliente2

--- GamaoFX/src/gamaofx/GamaoFX.java
+++ GamaoFX/src/gamaofx/GamaoFX.java
@@ -1,4 +1,4 @@
-package gamaofxclient;
+package gamaofx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,18 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class GamaoFXClient extends Application {
+public class GamaoFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gamaofx/scene.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
